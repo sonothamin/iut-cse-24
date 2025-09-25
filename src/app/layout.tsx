aee5ce2cc654a1f72a66a35b3ccd7984edd35b45
Sonoth Amin <76142440+sonothamin@users.noreply.github.com>
2025-09-26 03:27:18 +0600

    gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,14 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <script>
+        var cursor = document.querySelector('.gradient');
+        document.addEventListener('mousemove', function(e){
+        var x = e.clientX;
+        var y = e.clientY;
+        cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+      </script> 
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AuthProvider>
           <BootstrapClient />
